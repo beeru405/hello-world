311f2d33ee6237607ea437bc11724b8d48b1be31
Beeranna Dude <101712802+beeru405@users.noreply.github.com>
2024-01-29 10:58:48 +0530

    Update TestGreeter.java

--- server/src/test/java/com/example/TestGreeter.java
+++ server/src/test/java/com/example/TestGreeter.java
@@ -29,7 +29,7 @@ public class TestGreeter {
     greeter = new Greeter();
 
     // Set the path to your ChromeDriver executable
-    System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+    System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
 
     // Create a new instance of the Chrome driver
     driver = new ChromeDriver();
